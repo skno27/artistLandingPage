a157897e046c82684474d308b64460edcfb89fed
Deshon Morgan <deshonmorgan1317@gmail.com>
2024-11-25 20:54:19 -0600

    music plays quietly. removed unused assets

--- src/player.js
+++ src/player.js
@@ -1,39 +1,38 @@
-function playMedia(mediaSrc, mediaType, id) {
-  // Toggle view
-  var videoPlayer = document.getElementById(id);
-  console.log(videoPlayer);
-  // Get the computed style of the videoPlayer element
-  var computedStyle = window.getComputedStyle(videoPlayer);
+const playlist = [
+  "../static/229-lean_01.31.mp3",
+  "../static/232-In a Hurry (158 bpm SKNO)_02.03.mp3",
+  "../static/234-Adoration (158 bpm SKNO)_02.03.mp3",
+];
 
-  // Log the computed display style to the console
-  console.log(computedStyle.display);
+let currentSong = 0;
 
-  if (computedStyle.display === "none") {
-    videoPlayer.style.display = "block";
-    // videoPlayer.src = "./../static/lucky_dance.mp4";
-  } else {
-    videoPlayer.style.display = "none";
-    videoPlayer.pause();
-  }
+const audio = document.getElementById("background-audio");
 
-  var mediaPlayer = document.querySelector("#video-player", +mediaType);
-  mediaPlayer.src = mediaSrc;
-  mediaPlayer.play();
+audio.src = playlist[currentSong];
+audio.volume = 0.1;
+
+audio.addEventListener("ended", () => {
+  currentSong++;
+  if (currentSong >= playlist.length) currentSong = 0;
+  audio.src = playlist[currentSong];
+  audio.play().catch((error) => {
+    console.error("Error playing the next song", error);
+  });
+});
+
+const songTitleElement = document.createElement("p");
+document.getElementById("player").appendChild(songTitleElement);
+
+function updateSongTitle() {
+  const fileName =
+    "SKNO " + playlist[currentSong].split("/").pop().split("-")[0]; // Extract file name
+  songTitleElement.textContent = `Now Playing: ${fileName}`;
 }
 
-// display video if it is playing
-// function toggleVideo() {}
-//  controlling the media player
+audio.addEventListener("play", updateSongTitle);
 
-var audioPlayer = document.querySelector("#audio-player audio");
-var videoPlayer = document.querySelector("video-player video");
-
-// event listeners to control the players
-
-// audioPlayer.addEventListener("play", () => {
-//   console.log("Audio is playing");
-// });
-
-// videoPlayer.addEventListener("play", () => {
-//   console.log("Video is playing");
-// });
+window.addEventListener("click", () => {
+  audio.play().catch((error) => {
+    console.error("Autoplay error:", error);
+  });
+});
